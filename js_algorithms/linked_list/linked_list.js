dd5e8d26e0787b563f61982fbf9eedc3368d5154
carlpadilla <carlpadilla@protonmail.com>
2020-08-21 21:46:22 -0400

    added insert func

--- js_algorithms/linked_list/linked_list.js
+++ js_algorithms/linked_list/linked_list.js
@@ -36,6 +36,7 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  // append Func
   append(value) {
     const newNode = new Node(value);
 
@@ -44,6 +45,7 @@ class LinkedList {
     this.length++;
     return this;
   }
+  // prepend Func
   prepend(value) {
     const newNode = new Node(value);
 
@@ -52,11 +54,53 @@ class LinkedList {
     this.length++;
     return this;
   }
+  //  Print List Func
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  // Insert Func
+  insert(index, value) {
+    // Check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdPointer;
+    this.length++;
+    return this.printList;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  // remove Func
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+
+console.log(myLinkedList.printList());
 
 console.log(myLinkedList);
